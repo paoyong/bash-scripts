861b3bd8591c498e6671501581654e6ba2516a13
Keith Yong <terda12@gmail.com>
2014-09-28 02:46:47 -0400

    added readme and added some comments

---
+++ README.md
@@ -0,0 +1,3 @@
+chmod +x script.py
+./script.py
+
--- sprunge.py
+++ sprunge.py
@@ -1,4 +1,8 @@
 #!/usr/bin/env python
+#Requires internet connection
+#Some sample inputs: "ls -l", "echo "Hello world"
+#Returns a 'pastebin' link of the output of the input
+
 import subprocess
 command = input("Enter your command to be sprunged: ")
 subprocess.call(command + " | curl -F 'sprunge=<-' http://sprunge.us", shell=True)
